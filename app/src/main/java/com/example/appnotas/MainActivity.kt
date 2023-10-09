876099513d87558e8b80896b181a5ae19a17fcf7
Leopoldo Leonel Espino Gonzalez <Ls19120002@tecnm.surguanajuato.mx>
2023-10-08 23:46:46 -0600

    Commit boton add

--- app/src/main/java/com/example/appnotas/MainActivity.kt
+++ app/src/main/java/com/example/appnotas/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.appnotas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,21 +46,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyContent() {
-    Box(
-        modifier = Modifier.fillMaxSize()
-
+    Column(
+        modifier= Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.End
     ) {
-        StandardFAB(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        )
+        SmalledBtn()
+        Spacer(modifier = Modifier.height(8.dp))
+        ExtendedBtn()
     }
-}
+
+    }
+
 
 @Composable
-fun StandardFAB(modifier: Modifier = Modifier) {
-    FloatingActionButton(
+fun ExtendedBtn(modifier: Modifier = Modifier) {
+    ExtendedFloatingActionButton(
+        onClick = { /*TODO*/ },
+        modifier = modifier,
+        containerColor = MaterialTheme.colorScheme.primary
+    ) {
+        Text(text = "add something")
+        Icon(imageVector = Icons.Default.Add,
+            contentDescription = "Add",
+            tint= Color.White)
+
+    }
+
+}
+@Composable
+fun SmalledBtn(modifier: Modifier = Modifier) {
+    SmallFloatingActionButton(
         onClick = { /*TODO*/ },
         modifier = modifier,
         containerColor = MaterialTheme.colorScheme.primary
